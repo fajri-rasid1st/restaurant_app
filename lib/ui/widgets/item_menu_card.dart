91ee46dee7c99c00ba9860a88c763c33c7d8d29a
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2025-09-12 13:35:04 +0700

    update launcher icon and splash page

--- lib/ui/widgets/item_menu_card.dart
+++ lib/ui/widgets/item_menu_card.dart
@@ -1,58 +1,58 @@
-import 'package:flutter/material.dart';
-import 'package:restaurant_app/common/const/const.dart';
-import 'package:restaurant_app/data/models/item_menu.dart';
-import 'package:restaurant_app/ui/themes/color_scheme.dart';
-import 'package:restaurant_app/ui/widgets/custom_network_image.dart';
+// import 'package:flutter/material.dart';
+// import 'package:restaurant_app/common/const/const.dart';
+// import 'package:restaurant_app/data/models/item_menu.dart';
+// import 'package:restaurant_app/ui/themes/color_scheme.dart';
+// import 'package:restaurant_app/ui/widgets/custom_network_image.dart';
 
-class ItemMenuCard extends StatelessWidget {
-  final ItemMenu? food;
-  final ItemMenu? drink;
+// class ItemMenuCard extends StatelessWidget {
+//   final ItemMenu? food;
+//   final ItemMenu? drink;
 
-  const ItemMenuCard({Key? key, this.food, this.drink}) : super(key: key);
+//   const ItemMenuCard({Key? key, this.food, this.drink}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(0),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      clipBehavior: Clip.antiAlias,
-      child: Stack(
-        alignment: AlignmentDirectional.bottomStart,
-        children: <Widget>[
-          CustomNetworkImage(
-            imgUrl: food == null
-                ? Const.imgDrinkPlaceholder
-                : Const.imgFoodPlaceholder,
-            width: 300,
-            height: 300,
-            placeHolderSize: 40,
-          ),
-          Positioned.fill(
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: [
-                    Colors.black,
-                    Colors.transparent,
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Text(
-              drink?.name ?? food!.name,
-              style: Theme.of(context)
-                  .textTheme
-                  .titleSmall
-                  ?.copyWith(color: onPrimaryColor),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       margin: const EdgeInsets.all(0),
+//       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+//       clipBehavior: Clip.antiAlias,
+//       child: Stack(
+//         alignment: AlignmentDirectional.bottomStart,
+//         children: <Widget>[
+//           CustomNetworkImage(
+//             imgUrl: food == null
+//                 ? Const.imgDrinkPlaceholder
+//                 : Const.imgFoodPlaceholder,
+//             width: 300,
+//             height: 300,
+//             placeHolderSize: 40,
+//           ),
+//           Positioned.fill(
+//             child: Container(
+//               decoration: const BoxDecoration(
+//                 gradient: LinearGradient(
+//                   begin: Alignment.bottomCenter,
+//                   end: Alignment.topCenter,
+//                   colors: [
+//                     Colors.black,
+//                     Colors.transparent,
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(8),
+//             child: Text(
+//               drink?.name ?? food!.name,
+//               style: Theme.of(context)
+//                   .textTheme
+//                   .titleSmall
+//                   ?.copyWith(color: onPrimaryColor),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/ui/widgets/restaurant_card.dart
+++ lib/ui/widgets/restaurant_card.dart
@@ -1,118 +1,118 @@
-import 'package:flutter/material.dart';
-import 'package:restaurant_app/common/const/const.dart';
-import 'package:restaurant_app/utilities/utilities.dart';
-import 'package:restaurant_app/data/models/favorite.dart';
-import 'package:restaurant_app/data/models/restaurant.dart';
-import 'package:restaurant_app/ui/widgets/custom_network_image.dart';
+// import 'package:flutter/material.dart';
+// import 'package:restaurant_app/common/const/const.dart';
+// import 'package:restaurant_app/utilities/utilities.dart';
+// import 'package:restaurant_app/data/models/favorite.dart';
+// import 'package:restaurant_app/data/models/restaurant.dart';
+// import 'package:restaurant_app/ui/widgets/custom_network_image.dart';
 
-class RestaurantCard extends StatelessWidget {
-  final Restaurant? restaurant;
-  final Favorite? favorite;
+// class RestaurantCard extends StatelessWidget {
+//   final Restaurant? restaurant;
+//   final Favorite? favorite;
 
-  const RestaurantCard({
-    Key? key,
-    this.restaurant,
-    this.favorite,
-  }) : super(key: key);
+//   const RestaurantCard({
+//     Key? key,
+//     this.restaurant,
+//     this.favorite,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    final pictureId = restaurant?.pictureId ?? favorite!.pictureId;
-    final name = restaurant?.name ?? favorite!.name;
-    final city = restaurant?.city ?? favorite!.city;
-    final rating = restaurant?.rating ?? favorite!.rating;
-    final heroTag =
-        restaurant != null ? '${restaurant?.id}_res' : '${favorite!.id}_fav';
+//   @override
+//   Widget build(BuildContext context) {
+//     final pictureId = restaurant?.pictureId ?? favorite!.pictureId;
+//     final name = restaurant?.name ?? favorite!.name;
+//     final city = restaurant?.city ?? favorite!.city;
+//     final rating = restaurant?.rating ?? favorite!.rating;
+//     final heroTag =
+//         restaurant != null ? '${restaurant?.id}_res' : '${favorite!.id}_fav';
 
-    return Stack(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(16),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Hero(
-                    tag: heroTag,
-                    child: CustomNetworkImage(
-                      imgUrl: '${Const.imgUrl}/$pictureId',
-                      width: double.infinity,
-                      height: 100,
-                      placeHolderSize: 40,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        name,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleLarge
-                            ?.copyWith(fontSize: 18),
-                      ),
-                      const SizedBox(height: 12),
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.place_rounded,
-                            size: 20,
-                            color: Colors.red[400],
-                          ),
-                          const SizedBox(width: 4),
-                          Text(
-                            city,
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 2),
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.star_rate_rounded,
-                            size: 22,
-                            color: Colors.orange[400],
-                          ),
-                          const SizedBox(width: 4),
-                          Text(
-                            '$rating',
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Positioned.fill(
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: () {
-                return Utilities.navigateToDetailScreen(
-                  context: context,
-                  restaurant: restaurant,
-                  favorite: favorite,
-                );
-              },
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
+//     return Stack(
+//       children: <Widget>[
+//         Padding(
+//           padding: const EdgeInsets.all(16),
+//           child: Row(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: <Widget>[
+//               Expanded(
+//                 child: ClipRRect(
+//                   borderRadius: BorderRadius.circular(12),
+//                   child: Hero(
+//                     tag: heroTag,
+//                     child: CustomNetworkImage(
+//                       imgUrl: '${Const.imgUrl}/$pictureId',
+//                       width: double.infinity,
+//                       height: 100,
+//                       placeHolderSize: 40,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//               Expanded(
+//                 flex: 2,
+//                 child: Padding(
+//                   padding: const EdgeInsets.all(12),
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: <Widget>[
+//                       Text(
+//                         name,
+//                         maxLines: 2,
+//                         overflow: TextOverflow.ellipsis,
+//                         style: Theme.of(context)
+//                             .textTheme
+//                             .titleLarge
+//                             ?.copyWith(fontSize: 18),
+//                       ),
+//                       const SizedBox(height: 12),
+//                       Row(
+//                         children: <Widget>[
+//                           Icon(
+//                             Icons.place_rounded,
+//                             size: 20,
+//                             color: Colors.red[400],
+//                           ),
+//                           const SizedBox(width: 4),
+//                           Text(
+//                             city,
+//                             style: Theme.of(context).textTheme.bodyMedium,
+//                           )
+//                         ],
+//                       ),
+//                       const SizedBox(height: 2),
+//                       Row(
+//                         children: <Widget>[
+//                           Icon(
+//                             Icons.star_rate_rounded,
+//                             size: 22,
+//                             color: Colors.orange[400],
+//                           ),
+//                           const SizedBox(width: 4),
+//                           Text(
+//                             '$rating',
+//                             style: Theme.of(context).textTheme.bodyMedium,
+//                           )
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//         Positioned.fill(
+//           child: Material(
+//             color: Colors.transparent,
+//             child: InkWell(
+//               onTap: () {
+//                 return Utilities.navigateToDetailScreen(
+//                   context: context,
+//                   restaurant: restaurant,
+//                   favorite: favorite,
+//                 );
+//               },
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
